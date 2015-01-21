c9bd3467fd01ebeb522ebe1c24f6204d3de03c45
Thomas Boros <tomas.boros92@gmail.com>
2015-01-22 00:28:01 +0100

    much progress. Many TODOs

--- ryu/app/simple_switch_13.py
+++ ryu/app/simple_switch_13.py
@@ -79,6 +79,21 @@ class SimpleSwitch13(app_manager.RyuApp):
 
         datapath.send_msg(mod)
 
+    def prepare_match_action(self, pkt, parser, ofproto, direction='to_se'):
+        ipd = pkt.get_protocol(ipv4.ipv4)
+        tcpd = pkt.get_protocol(tcp.tcp)
+
+        if direction == 'to_se':
+            #TODO fix to_se direction
+            match = parser.OFPMatch(eth_type=0x800, ipv4_dst=ipd.src, ip_proto=6, tcp_dst=tcpd.src_port)
+        elif direction == 'from_se':
+            match = parser.OFPMatch(eth_type=0x800, ipv4_dst=ipd.src, ip_proto=6, tcp_dst=tcpd.src_port)
+
+        actions = [parser.OFPActionOutput(ofproto.OFPP_CONTROLLER, ofproto.OFPCML_NO_BUFFER)]
+
+        return match, actions
+
+
     def manage_cdncomm(self, pkt, ev):
         msg = ev.msg
         datapath = msg.datapath
@@ -92,20 +107,140 @@ class SimpleSwitch13(app_manager.RyuApp):
 
         tcpdat = pkt.get_protocol(tcp.tcp)
 
-        # SYN IS SET, MEANS WE ARE DECLARING A NEW SESSION AND STORE THE ORIGINAL PACKET
+        # Parsing of packet and getting payload
+        for p in pkt:
+            if isinstance(p, array.ArrayType):
+                payload = str(bytearray(p))
+        # Make sure variable payload is set
+        try:
+            payload
+        except NameError:
+            payload = None
+
+        # SYN IS SET, MEANS WE ARE DECLARING A NEW SESSION AND STORE THE ORIGINAL PACKET. SENDING A SYN ACK RESPONSE
         if tcpdat.bits & 0x2:
-            print 'SYN IS SET'
-            sess = Session(src_ip, tcpdat.src_port, pkt)
+            sess = Session(src_ip, tcpdat.src_port, pkt, dst_ip)
+            if src_ip in self.sessions:
+                self.sessions[src_ip][tcpdat.src_port] = sess
+            else:
+                self.sessions[src_ip] = {}
+                self.sessions[src_ip][tcpdat.src_port] = sess
+
+            #Generating ACK response to the TCP SYN
             ackpkt = sess.generateACKtoSYN()
             data = ackpkt.data
 
             actions = [parser.OFPActionOutput(in_port)]
-            out = parser.OFPPacketOut(datapath=datapath, buffer_id=ofproto.OFP_NO_BUFFER, in_port=ofproto.OFPP_CONTROLLER, actions=actions,
-                                      data=data)
+            out = parser.OFPPacketOut(datapath=datapath, buffer_id=ofproto.OFP_NO_BUFFER,
+                                      in_port=ofproto.OFPP_CONTROLLER, actions=actions, data=data)
             datapath.send_msg(out)
-            print 'PUT TO WIRE'
+            sess.setState(Session.SYNACKSENT)
+
+        # ACK IS SET
+        #TODO check this condition
+        if tcpdat.bits & 1 << 4:
+            try:
+                sess = self.sessions[src_ip][tcpdat.src_port]
+                if sess.getState() == Session.SYNACKSENT:
+                    # this is probably a ack to SYNACK
+                    # TODO parse, make sure it is a simple ACK for SYNACK
+                    if payload is None:
+                        sess.saveACKpkt(pkt)
+                        sess.setState(Session.ACKRECV)
+                        print 'ACK packet saved for our session. Waiting for HTTP GET'
+                        return
+                    else:
+                        #TODO raise own error, that the state is wrong
+                        print 'raise error here'
+                        return
+
+                if sess.getState() == Session.ACKRECV:
+                    # We should catch HTTP GET now
+                    if payload is not None:
+                        request = payload.split('\n', 1)[0]
+                        print 'Request is ', request
+                        sess.setState(Session.HTTPGETRECV)
+                        sess.setRequestURI(request)
+                        sess.saveHTTPGETpkt(pkt)
+                        sess.setPayload(payload)
+
+                        #TODO get SE and generate synpkt for it
+                        #TODO Install backwards flow mods, so we can catch the returning communication from server
+                        #se = sess.getServiceEngine()
+                        synpkt = sess.generateSYNpkt()
+
+                        #TODO flowmod for returning packets, then put it on wire
+                        match, actions = self.prepare_match_action(synpkt, parser, ofproto, 'from_se')
+
+                        self.add_flow(datapath, 2, match, actions)
+
+                        data = synpkt.data
+                        # TODO, get switch port
+                        actions = [parser.OFPActionOutput(1)]
+                        out = parser.OFPPacketOut(datapath=datapath, buffer_id=ofproto.OFP_NO_BUFFER,
+                                                  in_port=ofproto.OFPP_CONTROLLER, actions=actions, data=data)
+                        datapath.send_msg(out)
+                        sess.setState(Session.SYNSENT)
+
+                        return
+
+            except IndexError:
+                # TODO drop packet
+                print 'No session found'
+
+    def manage_backward_cdncomm(self, pkt, ev):
+        msg = ev.msg
+        in_port = msg.match['in_port']
+        datapath = msg.datapath
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+
+        ipd = pkt.get_protocol(ipv4.ipv4)
+        tcpdat = pkt.get_protocol(tcp.tcp)
+
+        sess = self.sessions[ipd.dst][tcpdat.dst_port]
+
+        print sess
+        print ipd
+        print tcpdat
+
+        if sess.getState() == Session.SYNSENT:
+            if tcpdat.bits == 1 << 4 | 1 << 1:
+                sess.setState(Session.SYNACKRECV)
+                sess.saveSEseq(tcpdat.seq)
+                sess.saveSYNACKpkt(pkt)
+
+                #GENERATE ACK TO SYN ACK
+                ackpkt = sess.generateACKtoSYNACK()
+
+                data = ackpkt.data
+
+                actions = [parser.OFPActionOutput(in_port)]
+                out = parser.OFPPacketOut(datapath=datapath, buffer_id=ofproto.OFP_NO_BUFFER,
+                                          in_port=ofproto.OFPP_CONTROLLER, actions=actions, data=data)
+                datapath.send_msg(out)
+
+                #TODO flowmod 2x for seq ip ack mac
+
+                #SRCmatch
+                match = parser.OFPMatch(eth_type=0x800, ipv4_src=sess.srcip, ip_proto=6, tcp_src=sess.srcport)
+                actions = [parser.OFPActionSetField(tcp_ack=1000), parser.OFPActionSetField(ipv4_dst='10.0.0.1'),
+                           parser.OFPActionSetField(eth_dst='08:00:27:f5:a4:ba'),
+                           parser.OFPActionOutput(1)]
+                self.add_flow(datapath, 3, match, actions)
+
+                #DSTmatch
+                match = parser.OFPMatch(eth_type=0x800, ipv4_dst=sess.srcip, ip_proto=6, tcp_dst=sess.srcport)
+                actions = [parser.OFPActionSetField(tcp_seq=1000), parser.OFPActionSetField(ipv4_src='10.0.0.5'),
+                           parser.OFPActionSetField(eth_src='00:11:22:33:44:55'),
+                           parser.OFPActionOutput(2)]
+                self.add_flow(datapath, 3, match, actions)
 
 
+
+                #TODO send HTTP GET
+
+                return
 
 
     @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
@@ -126,17 +261,21 @@ class SimpleSwitch13(app_manager.RyuApp):
         eth = pkt.get_protocols(ethernet.ethernet)[0]
 
         ipv4dat = pkt.get_protocol(ipv4.ipv4)
+        tcpdat = pkt.get_protocol(tcp.tcp)
         if ipv4dat is not None:
-            if ipv4dat.dst == "10.0.0.5":
-                tcpdat = pkt.get_protocol(tcp.tcp)
+            if ipv4dat.dst == "10.0.0.5":           # TODO IP FROM CONFIG FILE OR DB
+
                 if tcpdat is not None:
                     if tcpdat.dst_port == 80:
                         #TODO update
-                        print "we have a incoming TCP packet to port 80 on ip 10.0.0.5"
-                        print msg.match
-
                         self.manage_cdncomm(pkt, ev)
                         return
+            else:
+                if tcpdat is not None:
+                    if ipv4dat.dst in self.sessions:
+                        if tcpdat.dst_port in self.sessions[ipv4dat.dst]:
+                            self.manage_backward_cdncomm(pkt, ev)
+                            return
 
         dst = eth.dst
         src = eth.src
--- ryu/cdnapp/session.py
+++ ryu/cdnapp/session.py
@@ -5,17 +5,41 @@ from ryu.lib.packet import ethernet
 from ryu.lib.packet import tcp
 from ryu.lib.packet import ipv4
 
+
 class Session:
 
-    def __init__(self, srcip, srcport, pkt):
+    # FROM CLIENT STATES
+    SYNRECV = 1
+    SYNACKSENT = 2
+    ACKRECV = 3
+    HTTPGETRECV = 4
+
+    # TO CDN ENGINE STATES
+    SYNSENT = 6
+    SYNACKRECV = 7
+    ACKSENT = 8
+    HTTPGETSENT = 9
+
+    # FINAL STATES
+    SESSIONSJOINED = 10
+    CDNERROR = 11
+
+
+    def __init__(self, srcip, srcport, pkt, rrip):
         self.srcip = srcip
         self.srcport = srcport
-        self.pkt = pkt
         self.synpkt = pkt
+        self.seq = 0
+        self.requestRouterIP = None
+        self.ackpkt = None
+        self.requesturi = None
+        self.httpgetpkt = None
+        self.synackpkt = None
+        self.state = self.SYNRECV
 
+    #This function is used to generate a SYN, ACK response to a initial SYN request.
     def generateACKtoSYN(self):
-        for p in self.pkt:
-            print p
+        for p in self.synpkt:
             if p.protocol_name == 'ethernet':
                 eth_src = p.dst
                 eth_dst = p.src
@@ -40,4 +64,81 @@ class Session:
 
         return p
 
+    def getSYNpkt(self):
+        return self.synpkt
 
+    #TODO port and destination for SE
+    #def generateSYNpkt(self, dst_ip, dst_port):
+    def generateSYNpkt(self):
+        for p in self.synpkt:
+            if p.protocol_name == 'ethernet':
+                eth_dst = '08:00:27:f5:a4:ba'
+                e = ethernet.ethernet(eth_dst, p.src)
+            if p.protocol_name == 'ipv4':
+                ip_dst = '10.0.0.1'
+                ip = ipv4.ipv4(4, 5, p.tos, 0, p.identification, p.flags, 0, p.ttl, p.proto, 0, p.src, ip_dst, None)
+            if p.protocol_name == 'tcp':
+                #TODO change dstport
+                tcpd = tcp.tcp(p.src_port, p.dst_port, p.seq, p.ack, 0, p.bits, p.window_size, 0, p.urgent, str(bytearray(p.option)))
+
+        pkt = packet.Packet()
+        pkt.add_protocol(e)
+        pkt.add_protocol(ip)
+        pkt.add_protocol(tcpd)
+        pkt.serialize()
+
+        return pkt
+
+    def generateACKtoSYNACK(self):
+
+        ipack = self.ackpkt.get_protocol(ipv4.ipv4)
+        tcpack = self.ackpkt.get_protocol(tcp.tcp)
+
+        for p in self.synackpkt:
+            if p.protocol_name == 'ethernet':
+                e = ethernet.ethernet(p.src, p.dst)
+            if p.protocol_name == 'ipv4':
+                ip = ipv4.ipv4(4, 5, p.tos, 0, ipack.identification, ipack.flags, 0, ipack.ttl, ipack.proto, 0, p.dst, p.src)
+            if p.protocol_name == 'tcp':
+                tcpd = tcp.tcp(p.dst_port, p.src_port, p.ack, p.seq+1, 0, 1 << 4, tcpack.window_size, 0, 0)
+
+        pkt = packet.Packet()
+        pkt.add_protocol(e)
+        pkt.add_protocol(ip)
+        pkt.add_protocol(tcpd)
+        pkt.serialize()
+
+        return pkt
+
+    # This function is used to save the incoming ACK packet for our SYN, ACK for later use
+    def saveACKpkt(self, pkt):
+        self.ackpkt = pkt
+        return
+
+    def setState(self, state):
+        self.state = state
+        print 'Session state of ', self.srcip, ':', self.srcport, ': ', self.state
+        return
+
+    def getState(self):
+        return self.state
+
+    def setRequestURI(self, uri):
+        self.requesturi = uri
+        #TODO calculate best SERVICE ENGINE, SE
+
+    def getServiceEngine(self):
+        #TODO, do real calculation here
+        return "10.0.0.1"
+
+    def setPayload(self, payload):
+        self.payload = payload
+
+    def saveHTTPGETpkt(self, pkt):
+        self.httpgetpkt = pkt
+
+    def saveSYNACKpkt(self, pkt):
+        self.synackpkt = pkt
+
+    def saveSEseq(self, seq):
+        self.seq = seq
